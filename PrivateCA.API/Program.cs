287b7a573e87b7b89589316ce36d3836c4c9af35
jas20202 <99250573+jas20202@users.noreply.github.com>
2024-03-23 12:59:56 +0100

    removed codecomments

--- PrivateCA.API/Program.cs
+++ PrivateCA.API/Program.cs
@@ -23,53 +23,9 @@ var capath = app.Configuration["capath"]!;
 var caName = app.Configuration["CaName"]!;
 var privateCA = new CA(workerpath, capath, caName);
 
-//async Task<string> GetCertContentAsync(CsrDTO data, string workingOn) {
-//    var csrPath = Path.Combine(workingOn, "csrContent");
-//    var extPath = Path.Combine(workingOn, "extContent");
-
-//    await File.WriteAllTextAsync(csrPath, data.CsrContent);
-//    await File.WriteAllTextAsync(extPath, data.ExtContent);
-
-//    var outPath = Path.Combine(workingOn, "certContent");
-
-//    OpenSSL.SignCSRWithCAKey(csrPath, extPath, outPath, caPath, caName, data.Password);
-
-//    var certContent = await File.ReadAllTextAsync(outPath);
-//    return certContent;
-//}
-
-//void CheckIfCaExists() {
-//    var caKeyName = OpenSSL.GetCAKeyName(caName, caPath);
-//    var caCertName = OpenSSL.GetCACertName(caName, caPath);
-//    var password = app.Configuration["Password"]!;
-
-//    if (!Path.Exists(caKeyName)) {
-//        OpenSSL.GenerateCAPrivateKey(caName, caPath, password);
-//    }
-
-//    if (!Path.Exists(caCertName)) {
-//        OpenSSL.GenerateRootCertificate(caName, caPath, app.Configuration["Issuer"]!, password);
-//    }
-//}
-
 app.UseHttpsRedirection();
 
 app.MapPost("/signcsr", async ([FromBody]CsrDTO data) => {
-    //CheckIfCaExists();
-
-    //var guid = Guid.NewGuid().ToString();
-    //var workingOn = Path.Combine(workerpath, guid);
-    //var certContent = "";
-
-    //Directory.CreateDirectory(workingOn);
-
-    //try {
-    //    certContent = await GetCertContentAsync(data, workingOn);
-    //} finally {
-    //    Directory.Delete(workingOn, true);
-    //}
-
-    //return new CsrResponseDTO(certContent);
     privateCA.CheckIfCaExists(app.Configuration["Issuer"]!, app.Configuration["Password"]!);
     return privateCA.Sign(data);
 });
